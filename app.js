75c383ab80e7e732ce1060114d6d13fa978f38a2
LucasKoval <lucaskoval@hotmail.com>
2020-10-19 10:15:10 -0300

    Modificado header y footer index.html

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+// Requerir las librerías Express y fs
 const express = require('express');
+const fs = require('fs');
+
+// Declarar la app de Express y el puerto
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-
+// Levantar el servidor local
 app.listen(port, () => {
     console.log('Servido corriendo en: http://localhost:3000/');
 });
 
+// Definir la carpeta 'public' como root
+app.use(express.static('public'));
+
+// Métodos GET para las páginas del sitio
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 });
