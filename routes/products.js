9e9f58b1a5952202eb56acb5dc41e50124b9bf57
JulietaRmz <julieta.retamoza@gmail.com>
2020-11-20 14:18:02 -0300

    modificaciones rutas y views

--- routes/products.js
+++ routes/products.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const productsController = require('../controllers/productsController');
 
 //----------* PRODUCTS ROUTES *----------//
-router.get('/', productsController.list);              //-> Listar productos 
-router.get('/sale', productsController.sale);          //-> Listar productos sale
-router.get('/:id', productsController.detail); //-> Detalle  de producto
-router.get('/carrito', productsController.cart);          
-router.get('/crear', productsController.createForm);
-//router.post('/crear', productsController.create);
-router.get('/:id/editar', productsController.edit);
+router.get('/', productsController.list);              //-> Listar productos Colección
+router.get('/sale', productsController.sale);          //-> Listar productos Sale
+router.get('/:id', productsController.detail);         //-> Detalle de producto
+router.get('/carrito', productsController.cart);       //-> Carrito   
+router.get('/crear', productsController.createForm);   //-> Formulario de creación
+//router.post('/crear', productsController.store);     //-> Guardar el producto
+router.get('/:id/editar', productsController.editForm);    //-> Mostrar formulario de edición un producto
+//router.put('/:id/editar', productsController.edit);  //-> Editar un producto
+//router.delete('/:id', productsController.delete);    //-> Borrar un producto
 
 
 //----------* EXPORTS ROUTER *----------//
