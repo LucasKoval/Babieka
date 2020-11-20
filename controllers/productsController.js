fad50823c99dc30c9d1d688804af9759a2ce93ee
JulietaRmz <julieta.retamoza@gmail.com>
2020-11-20 17:50:50 -0300

    método edición

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -42,6 +42,8 @@ const productsController = {
 			casualProducts: casual
 		});        
     },
+
+
     //Renderiza la vista Sale
     sale: (req, res) => {        
         const products = getAllProducts();
@@ -53,26 +55,62 @@ const productsController = {
 			saleProducts: sale
 		});  
     },
+
+
     //Renderiza la vista Detalle de producto
     detail: (req, res) => {   
         let productos = getAllProducts();
         let producto = productos.find(elemento => elemento.id == req.params.id);     
         res.render('products/productDetail', { producto : producto });
     },
+
+
     //Renderiza la vista Carrito
     cart: (req, res) => {        
         res.render('products/productCart');
     },
+
+
     //Renderiza la vista Nuevo artículo
     createForm: (req, res) => {        
         res.render('products/createProduct');
     },
+
+
+    //Crear Nuevo artículo
+    store: (req, res) => {        
+        res.render('products/createProduct');
+    },
+
+
     //Renderiza la vista Edición de artículo
     editForm: (req, res) => { 
         let productos = getAllProducts();
         let producto = productos.find(elemento => elemento.id == req.params.id);     
         res.render('products/editProduct', { producto : producto });       
     },
+
+
+    //Editar artículo (PUT)
+    edit: (req, res) => {        
+        let productos = getAllProducts();
+        const productoEditado = productos.map(function(producto){
+            if (producto.id == req.params.id) {
+                producto.name=req.body.name;
+                producto.type=req.body.type;
+                producto.size=req.body.size;
+                producto.color=req.body.color;
+                producto.description=req.body.description;
+                //producto.image=req.body.image;
+                producto.price=req.body.price;
+            } 
+            return producto
+        })
+
+        writeProducts(productoEditado);
+        res.redirect('/producto/'+ req.params.id);
+    },
+
     //Elimina el registro de un artículo
     delete: (req, res) => {        
         //obtener el producto del id del req
