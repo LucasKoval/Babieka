e9cf02d0c1a35269ae82ee4c51c1941ec1f1f44d
fedegox <fedegox775@gmail.com>
2020-11-23 13:15:12 -0300

    arreglosRoutes

--- routes/products.js
+++ routes/products.js
@@ -6,14 +6,13 @@ const productsController = require('../controllers/productsController');
 //----------* PRODUCTS ROUTES *----------//
 router.get('/', productsController.list);                  //-> Listar productos Colección
 router.get('/sale', productsController.sale);              //-> Listar productos Sale
-router.get('/:id', productsController.detail);             //-> Detalle de producto
 router.get('/carrito', productsController.cart);           //-> Carrito   
 router.get('/crear', productsController.createForm);       //-> Formulario de creación
 //router.post('/crear', productsController.store);         //-> Guardar el producto
 router.get('/:id/editar', productsController.editForm);    //-> Mostrar formulario de edición un producto
 router.put('/:id/editar', productsController.edit);      //-> Editar un producto
 //router.delete('/:id', productsController.delete);        //-> Borrar un producto
-
+router.get('/:id', productsController.detail);             //-> Detalle de producto
 
 //----------* EXPORTS ROUTER *----------//
 module.exports = router;
