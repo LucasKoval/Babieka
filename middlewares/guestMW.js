f2bff2f6805d3c08dcba0b108276641849b24eb2
JulietaRmz <julieta.retamoza@gmail.com>
2020-12-23 00:39:46 -0300

    arreglo de userList, creación usuario profe y agrego toLowerCase

--- middlewares/guestMW.js
+++ middlewares/guestMW.js
@@ -1,6 +1,6 @@
 //----------* MIDDLEWARE *----------//
 module.exports = (req, res, next) => {
-    if (req.session.user) {
+    if (req.session.user && req.session.user.category != 'admin') {
         res.redirect('/usuario/perfil');
     }
     return next();
