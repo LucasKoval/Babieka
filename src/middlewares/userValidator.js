3fcb6bb300416eb2f12a7d5d3fd8054f8a4aaf58
fedegox <fedegox775@gmail.com>
2021-02-04 02:50:14 -0300

    revision

--- src/middlewares/userValidator.js
+++ src/middlewares/userValidator.js
@@ -53,14 +53,14 @@ module.exports = {
             .notEmpty()
                 .withMessage('Debes repetir tu contraseña')
                 .bail(),
-        body('role')
+        /*body('role')
             .custom(function(value, {req}){
                 if (req.session.user){
                      return value != undefined
                 }
             })
             .withMessage('Debes seleccionar un rol')
-            .bail(),
+            .bail(),*/
         body('image')
             .custom(function(value, { req }){
                 return req.files[0];
